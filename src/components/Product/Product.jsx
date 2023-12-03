4d8ca7c8316bebcff561e444b17472a12243916d
IgorGazineo <gaumgazineo@hotmail.com>
2023-12-03 11:06:00 -0300

    ajustando o tamanho dos produtos no card

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,10 +2,16 @@ import "./Product.css";
 
 export default function Product({ title, description, image }) {
   return (
-    <div className="product">
-      <img className="products" src={image} alt={title} />
-      <h4 className="products">{title}</h4>
-      <p className="products">{description}</p>
+    <div className="product-card">
+      <div className="product">
+        <div className="product-left">
+          <img className="product-img" src={image} alt={title} />
+        </div>
+        <div className="product-right">
+          <h4 className="product-title">{title}</h4>
+          <p className="product-description">{description}</p>
+        </div>
+      </div>
     </div>
   );
 }
--- src/data.js
+++ src/data.js
@@ -7,25 +7,29 @@ export const PRODUCTS_DATA = [
   {
     id: "i1",
     title: "Smartphone",
-    description: "Em breve",
+    description:
+      "Um universo de possibilidades na palma da sua mão. Com uma câmera incrível, tela de alta resolução e desempenho ágil.",
     image: smartphoneImage,
   },
   {
     id: "i2",
     title: "Notebook",
-    description: "Em breve",
+    description:
+      "Nosso laptop combina desempenho poderoso, design compacto e uma tela envolvente para acompanhar você em qualquer lugar.",
     image: notebookImage,
   },
   {
     id: "i3",
     title: "TV",
-    description: "Em breve",
+    description:
+      "Transforme sua sala em um cinema particular. Com cores vibrantes, qualidade de imagem incrível e acesso a uma infinidade de conteúdos.",
     image: tvImage,
   },
   {
     id: "i4",
     title: "Headephone",
-    description: "Em breve",
+    description:
+      "Desfrute de uma experiência sonora envolvente com nosso headphone premium. Com áudio de alta qualidade e cancelamento de ruído.",
     image: headphoneImage,
   },
 ];
