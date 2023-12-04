aefe2c243645f0166d82805248fed45790dd96dd
IgorGazineo <gaumgazineo@hotmail.com>
2023-12-04 02:09:58 -0300

    melhorando a responsividade do aplicativo

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,17 @@ import { PRODUCTS_DATA } from "./data.js";
 
 function App() {
   const [productValue, setProductValue] = useState(null);
+  const [animate, setAnimate] = useState(true);
   let productContent = <h3>Selecione um produto</h3>;
 
   if (productValue !== null) {
-    productContent = <Product {...PRODUCTS_DATA[productValue]} />;
+    productContent = (
+      <Product {...PRODUCTS_DATA[productValue]} isTrue={animate} />
+    );
   }
   const handleClick = function (ident) {
     setProductValue(ident);
+    setAnimate(!animate);
   };
   return (
     <div>
--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,13 +1,15 @@
 import "./Product.css";
 
-export default function Product({ title, description, image }) {
+export default function Product({ title, description, image, isTrue }) {
   return (
     <div className="product-card">
       <div className="product">
         <div className="product-left">
           <img className="product-img" src={image} alt={title} />
         </div>
-        <div className="product-right">
+        <div
+          className={`product-right ${isTrue ? "animation" : "reanimation"}`}
+        >
           <h4 className="product-title">{title}</h4>
           <p className="product-description">{description}</p>
         </div>
